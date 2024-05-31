900a813c73e8d9c7f408dafa6019d6a580f46579
Anastasia <whydema@gmail.com>
2024-05-31 23:01:29 +0300

    first

---
+++ src/main/java/org/example/buysell/repositories/ImageRepository.java
@@ -0,0 +1,7 @@
+package org.example.buysell.repositories;
+
+import org.example.buysell.models.Image;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface ImageRepository extends JpaRepository<Image, Long> {
+}
